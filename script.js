4a52bdfd1f6317b2803c5667ccc908c8ac2df143
sgueissaz <36111447+sgueissaz@users.noreply.github.com>
2026-03-15 00:03:55 +0100

    Add button to start and random color

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ const ONLY_NUMBER = /^[0-9]+$/;
 
 const state = {
     sizeGrid: 0,
-    isDrawing: false
+    isDrawing: false,
+    color: {
+        red: 0,
+        green: 0,
+        blue: 0,
+    },
+    squares: new Map()
 };
+
+function getSquareId(x, y) {
+    return `square-${x}-${y}`;
+}
+
+function toggleDrawing() {
+    state.isDrawing = !state.isDrawing;
+}
+
+function setRandomColor() {
+    state.color = {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256)
+    };
+}
 
 function getSizeGrid() {
     let sizeGrid = 0;
@@ -14,27 +36,55 @@ function getSizeGrid() {
         }
         sizeGrid = parseInt(response);
     } while (sizeGrid < 1 || sizeGrid > 100);
-    return sizeGrid;
+
+    state.sizeGrid = sizeGrid;
+}
+
+function initGrid() {
+    state.squares = new Map();
+
+    for (let i = 0; i < state.sizeGrid; i++) {
+        for (let j = 0; j < state.sizeGrid; j++) {
+            const square = {
+                opacity: 0,
+                color: {}
+            };
+            state.squares.set(getSquareId(i, j), square);            
+        }
+    }
+}
+
+function drawSquare(squareId) {
+    const square = state.squares.get(squareId);
+    if (square === undefined) {
+        return;
+    }
+
+    square.color = state.color;
+
+    if (square.opacity < 1) {
+        square.opacity += 0.1;
+    }
 }
 
 const ui = {
     html: document.querySelector('html'),
+    start: document.querySelector('#start'),
     grid: document.querySelector('.grid'),
     squares: new Map()
 };
 
-function paintSquare(squareId) {
-    ui.squares.get(squareId).classList.add('black');
-}
+function initGridUI() {
+    ui.grid.innerHTML = '';
+    ui.squares = new Map();
 
-function initGrid(sizeGrid) {
-    for (let i = 0; i < sizeGrid; i++) {
+    for (let i = 0; i < state.sizeGrid; i++) {
         const rowElement = document.createElement('div');
         rowElement.classList.add('row');
         
-        for (let j = 0; j < sizeGrid; j++) {
+        for (let j = 0; j < state.sizeGrid; j++) {
             const squareElement = document.createElement('div');
-            squareElement.id = `square-${i}-${j}`;
+            squareElement.id = getSquareId(i, j);
             squareElement.classList.add('square');
 
             rowElement.appendChild(squareElement);
@@ -45,20 +95,47 @@ function initGrid(sizeGrid) {
     }
 }
 
+function getColorCSS(color) {
+    return `rgb(${color.red}, ${color.green}, ${color.blue})`;
+}
+
+function drawSquareUI(squareId) {
+    const square = state.squares.get(squareId);
+    if (square === undefined) {
+        return;
+    }
+
+    const squareUI = ui.squares.get(squareId);
+    if (squareUI === undefined) {
+        return;
+    }
+
+    squareUI.style.opacity = square.opacity;
+    squareUI.style.backgroundColor = getColorCSS(square.color);
+}
+
+ui.start.addEventListener('click', () => {
+    getSizeGrid();
+    initGrid();
+    initGridUI();
+})
+
 ui.html.addEventListener('pointerdown', (e) => {
-    state.isDrawing = true;
+    toggleDrawing();
+    setRandomColor();
     e.preventDefault();
 });
+
 ui.grid.addEventListener('pointermove', (e) => {
     if (!state.isDrawing) {
         return;
     }
-    paintSquare(e.target.id);
-});
-ui.html.addEventListener('pointerup', (e) => {
-    state.isDrawing = false;
-    e.preventDefault();
+
+    drawSquare(e.target.id);
+    drawSquareUI(e.target.id);
 });
 
-state.sizeGrid = getSizeGrid();
-initGrid(state.sizeGrid);
+ui.html.addEventListener('pointerup', (e) => {
+    toggleDrawing();
+    e.preventDefault();
+});
